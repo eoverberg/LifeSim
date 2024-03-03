69fe846deaaca7cba9a6735d11354b3337f44795
Erik <eoverberg3461@gmail.com>
2024-03-02 23:34:11 -0600

    Adds skeleton global class

---
+++ backend/src/Global.js
@@ -0,0 +1,39 @@
+class Global {
+    constructor() {
+        this.intGrazerCount = 0;
+        this.intPredCount = 0;
+        this.intPlantCount = 0;
+        this.intObsCount = 0;
+        this.size = 0;
+        this.time = 0.0;
+        this.plantList = [];
+        this.predList = [];
+        this.grazerList = [];
+        this.obsList = [];
+    }
+
+    newPlant() {
+        // Implement newPlant method logic here
+    }
+
+    newGrazer() {
+        // Implement newGrazer method logic here
+    }
+
+    newPredator() {
+        // Implement newPredator method logic here
+    }
+
+    newObs() {
+        // Implement newObs method logic here
+    }
+
+    initializeWorld() {
+        // Implement initializeWorld method logic here
+        this.worldMatrix = new Array[this.size, this.size]
+    }
+
+    setWorld() {
+        // Implement setWorld method logic here
+    }
+}

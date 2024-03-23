666f807a74686342f8fd2abf3235d8bfae011617
Adam Johnson <81100336+amjohns1@users.noreply.github.com>
2024-03-23 17:12:43 -0500

    Revert "Obstacle, Plant and UtilitiesFunc"

--- backend/src/ObstacleClass.js
+++
@@ -1,8 +0,0 @@
-import { isColliding } from './utilities';
-
-class Obstacle extends Entity {
-    constructor(xPos,yPos,zPosm,lifeTime) {
-        super(xPos,yPos,zPosm,lifeTime);
-        this.size = size; // Obstacle size, could allow entities to go around it
-    }
-}

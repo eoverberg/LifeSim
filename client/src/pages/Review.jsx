44859c28420325534f10b59ed0bbe49c9de4b79d
RileyRhodes283 <rhodesfam283@gmail.com>
2024-04-09 11:22:44 -0500

    2nd push

--- client/src/pages/Review.jsx
+++ client/src/pages/Review.jsx
@@ -1,7 +1,18 @@
+import { useLocalStorage } from "../components/useLocalStorage";
+import GetStatistic from "../components/getStatistics";
+import DisplayStatistics from "../components/displayStatistics";
 const Review = () => {
-
-    return<h><p>Put Top Scores Here</p></h>;
-
+     const [statistics, setStatistics] = useLocalStorage("stats", "default");
+    //setStatistics = {setStats}
+    //<GetStats> </GetStats>
+    return(<h>
+        <p>Top Scores:</p>
+        <GetStatistic setStatistics = {setStatistics}/>
+        <DisplayStatistics statistics = {statistics}/>
+        <p>{setStatistics}</p>
+        <p>THis is a tester</p>
+    </h>);
+    
 };
 
 export default Review;

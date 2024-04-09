a3eb00afac198ff1a593ee6c9a2dbaf215c82f1a
Riley <rhodesfam283@gmail.com>
2024-04-09 13:57:26 -0500

    Dropped Obstacle Counter

--- client/src/components/InitStats.jsx
+++ client/src/components/InitStats.jsx
@@ -9,15 +9,13 @@ function InitStats({displayData}){
         let num_plants = parseInt(lines[2]);
         let num_grazers = parseInt(lines[4]);
         let num_predators = parseInt(lines[3]);
-        let num_obstacles = parseInt(lines[5]);
 
 
 //        }, [displayData,num_plants,num_obstacles,num_grazers,num_predators]);
         return (<>
             <b class = "plantCounter"> Number of Plants: {num_plants} </b>
             <b class = "grazerCounter"> Number of Grazers: {num_predators}</b>     
-            <b class = "predatorCounter"> Number of Predators: {num_grazers}</b>     
-            <b class = "obstaclesCounter"> Number of Obstacles: {num_obstacles}</b>     
+            <b class = "predatorCounter"> Number of Predators: {num_grazers}</b>        
             </>
           );
 }

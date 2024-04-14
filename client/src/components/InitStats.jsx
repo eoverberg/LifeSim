69faf33497cfc460463df9a3f4b03090e7c04e44
Riley <rhodesfam283@gmail.com>
2024-04-14 13:54:19 -0500

    Fixed bug inside InitStats

--- client/src/components/InitStats.jsx
+++ client/src/components/InitStats.jsx
@@ -1,15 +1,21 @@
 import { useRef, useEffect } from 'react';
+let num_plants = 0;
+let num_grazers = 0;
+let num_predators = 0;
 
 function InitStats({displayData}){
 
-
 //    useEffect(() => {   
-        
+        try {
+                displayData.split(',')
+        }catch(error)
+        {
+                return 
+        }
         let lines = displayData.split(',')
         let num_plants = parseInt(lines[2]);
         let num_grazers = parseInt(lines[4]);
         let num_predators = parseInt(lines[3]);
-
 
 //        }, [displayData,num_plants,num_obstacles,num_grazers,num_predators]);
         return (<>

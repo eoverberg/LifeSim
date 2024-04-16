cdcfc18d6185a5ed3ea20d8b0d03bfa95f8a5da8
Riley <rhodesfam283@gmail.com>
2024-04-16 17:47:49 -0500

    deleted redundance

--- client/src/pages/Review.jsx
+++ client/src/pages/Review.jsx
@@ -3,15 +3,11 @@ import GetStatistic from "../components/GetStats";
 import DisplayStatistics from "../components/DisplayStats";
 const Review = () => {
      const [statistics, setStatistics] = useLocalStorage("stats", "default");
-    //setStatistics = {setStats}
-    //<GetStats> </GetStats>
     return(
     <h>
         <p>Top Scores:</p>
         <GetStatistic setStatistics = {setStatistics}/>
         <DisplayStatistics statistics = {statistics}/>
-        <p>{setStatistics}</p>
-        <p>THis is a tester</p>
     </h>
     );
     
